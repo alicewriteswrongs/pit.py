dff0ec1517790ba920d59e0af50d9a6c43c83455
Alice <alice.writes.wrongs@gmail.com>
2015-07-10 13:21:21 -0400

    Can add objects to the database! woo

--- version.py
+++ version.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import hashlib
+import time
 
 def main():
     if (len(sys.argv) == 1 or sys.argv[1] == "--help"):
@@ -21,35 +22,48 @@ def main():
             return 0
     elif (sys.argv[1] == "--add"):
         if (os.path.isfile("./" + sys.argv[2])):
-            filename = sys.argv[2]
-            filedir = hashlib.sha1()
-            filedir.update(filename.encode('utf-8'))
-            if (os.path.isdir("./.versionpy/objects/" + filedir.hexdigest())):
-                print("yup, in there")
-            else:
-                os.mkdir("./.versionpy/objects/" + filedir.hexdigest())
-                print("made it?")
-
-                    
-
-
-
-
-
-
-
-
-            print("it's a file all right")
+            # if file is in cwd then we can check to see if it's already in objects
+            addFile(sys.argv[2])
         else:
-            print("that doesn't make sense to me")
-        filepath = "." + sys.argv[2]
+            print("error: not a file", file=sys.stderr)
     else:
-        print("oops")
-        
+        print("sorry, I didn't understand that", file=sys.stderr)
+
+
             
+def addFile(filename):
+    """
+    checks to see if a file is already in the object directory,
+    if it is it then checks current version against this one,
+    and decides if it will add a new version or not.
 
+    if the file is not in the directory at all, then it adds a
+    new object folder, and add the first version of the file to it
+    """
+    print(filename)
+    filedir = hashlib.sha1()
+    filedir.update(filename.encode('utf-8'))
+    # no object dir? make it
+    if not (os.path.isdir("./.versionpy/objects/" + filedir.hexdigest())):
+        os.mkdir("./.versionpy/objects/" + filedir.hexdigest())
 
-        
+    hashname = hashlib.sha1()
+    moddate = time.ctime(os.path.getmtime(filename)).encode('utf-8')
+    hashname.update(filename.encode('utf-8') + moddate)
+    if not (os.path.isfile("./.versionpy/objects/" + filedir.hexdigest() 
+                    + "/" + hashname.hexdigest())):
+        # we open a file in the added files object directory,
+        # with the name of the filename + last date modified
+        writefile = open("./.versionpy/objects/" + filedir.hexdigest()
+            + "/" + hashname.hexdigest(),"w")
+        with open(sys.argv[2]) as myfile:
+            text = myfile.read()
+
+        writefile.write(text)
+        writefile.close()
+
+    else: #this means the file with that name and date modified already exists
+        print("error: that version is already in database", file=sys.stderr)
 
 
 main()
