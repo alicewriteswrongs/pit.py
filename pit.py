96d08be9788b12992b19728f331b03b2c5fe8b36
Alice <alice.writes.wrongs@gmail.com>
2015-07-10 16:24:21 -0400

    Changing the name!

--- pit.py
+++ pit.py
@@ -6,12 +6,12 @@ import json
 
 def main():
     if (len(sys.argv) == 1 or sys.argv[1] == "--help"):
-        print("usage:  version.py --init\t\tstart a new repo")
-        print("\tversion.py --add\t\tstage a file")
-        print("\tversion.py --commit author message\twrite a commit with staged files")
-        print("\tversion.py --status\t\tget information about staged files")
-        print("\tversion.py --info\t\tget information about a commit")
-        print("\tversion.py --help to display this message")
+        print("usage:  pit.py --init\t\tstart a new repo")
+        print("\tpit.py --add\t\tstage a file")
+        print("\tpit.py --commit author message\twrite a commit with staged files")
+        print("\tpit.py --status\t\tget information about staged files")
+        print("\tpit.py --info\t\tget information about a commit")
+        print("\tpit.py --help to display this message")
         return 0
     elif (sys.argv[1] == "--init"):
         init()
@@ -31,11 +31,11 @@ def main():
 
 
 def status():
-    with open("./.versionpy/stage") as stage:
+    with open("./.pit/stage") as stage:
         text = stage.readlines()
 
     if (text == []):
-        print("Nothing staged. Use python version.py --add myfile")
+        print("Nothing staged. Use python pit.py --add myfile")
     else:
         print("Files currently staged for commit:")
         for line in text:
@@ -47,16 +47,16 @@ def init():
     this just checks for an existing repo, and if there isn't one it
     will make a fresh one
     """
-    if (os.path.isdir("./.versionpy")):
+    if (os.path.isdir("./.pit")):
         print("error: repository exists", file = sys.stderr)
         return -1
     else:
-        os.mkdir("./.versionpy")
-        os.mkdir("./.versionpy/objects")
-        os.mkdir("./.versionpy/commits")
-        open("./.versionpy/stage","w")
-        open("./.versionpy/head","w")
-        print("created empty repository in .versionpy, enjoy!")
+        os.mkdir("./.pit")
+        os.mkdir("./.pit/objects")
+        os.mkdir("./.pit/commits")
+        open("./.pit/stage","w")
+        open("./.pit/head","w")
+        print("created empty repository in .pit, enjoy!")
         return 0
 
 
@@ -65,13 +65,13 @@ def stageFile(filename, filedir, hashname):
     this is called within addFile, and it will add the appropriate info
     to the stage file
     """
-    with open("./.versionpy/stage") as myfile:
+    with open("./.pit/stage") as myfile:
         lines = myfile.readlines()
 
-    filepath = "./.versionpy/objects/" + filedir.hexdigest() + "/" + hashname.hexdigest()
+    filepath = "./.pit/objects/" + filedir.hexdigest() + "/" + hashname.hexdigest()
 
     if (lines == []):
-        with open("./.versionpy/stage", "a") as myfile:
+        with open("./.pit/stage", "a") as myfile:
             myfile.writelines(filename + '\t' + filepath + "\n")
     else:
         for line in lines:
@@ -79,7 +79,7 @@ def stageFile(filename, filedir, hashname):
                 print("error: object already staged but not in object directory", file=sys.stderr)
                 return -1
 
-        with open("./.versionpy/stage", "a") as myfile:
+        with open("./.pit/stage", "a") as myfile:
             myfile.writelines(filename + '\t' + filepath + "\n")
 
 
@@ -94,11 +94,11 @@ def writeCommit(author, message):
     commit['author'] = author
 
     #get parent
-    with open("./.versionpy/head", "r") as myfile:
+    with open("./.pit/head", "r") as myfile:
         commit['parent'] = myfile.read()
 
     #read in staged files
-    with open("./.versionpy/stage", "r") as myfile:
+    with open("./.pit/stage", "r") as myfile:
         files = myfile.readlines()
 
     committed = {}
@@ -110,17 +110,17 @@ def writeCommit(author, message):
     commit['committed_files'] = committed
 
     #reset stage file
-    with open("./.versionpy/stage", "w") as myfile:
+    with open("./.pit/stage", "w") as myfile:
         myfile.write("")
 
     #write the commit file
     filename = hashlib.sha1()
     filename.update(message.encode('utf-8') + author.encode('utf-8') + time.ctime().encode('utf-8'))
-    with open("./.versionpy/commits/" + filename.hexdigest(), "w") as myfile:
+    with open("./.pit/commits/" + filename.hexdigest(), "w") as myfile:
         json.dump(commit, myfile)
 
     #update head file (filename of latest commit)
-    with open("./.versionpy/head","w") as myfile:
+    with open("./.pit/head","w") as myfile:
         myfile.write(filename.hexdigest())
 
     print("Commited " + str(filecounter) + " file(s) to " + filename.hexdigest())
@@ -129,7 +129,7 @@ def commitInfo(commitname):
     """
     get info about a commit!
     """
-    with open("./.versionpy/commits/" + commitname) as myfile:
+    with open("./.pit/commits/" + commitname) as myfile:
         commit = json.load(myfile)
 
     print("Commited by " + commit['author'] + ". Message: " + commit['message'])
@@ -153,17 +153,17 @@ def addFile(filename):
     filedir = hashlib.sha1()
     filedir.update(filename.encode('utf-8'))
     # no object dir? make it
-    if not (os.path.isdir("./.versionpy/objects/" + filedir.hexdigest())):
-        os.mkdir("./.versionpy/objects/" + filedir.hexdigest())
+    if not (os.path.isdir("./.pit/objects/" + filedir.hexdigest())):
+        os.mkdir("./.pit/objects/" + filedir.hexdigest())
 
     hashname = hashlib.sha1()
     moddate = time.ctime(os.path.getmtime(filename)).encode('utf-8')
     hashname.update(filename.encode('utf-8') + moddate)
-    if not (os.path.isfile("./.versionpy/objects/" + filedir.hexdigest() 
+    if not (os.path.isfile("./.pit/objects/" + filedir.hexdigest() 
                     + "/" + hashname.hexdigest())):
         # we open a file in the added files object directory,
         # with the name of the filename + last date modified
-        writefile = open("./.versionpy/objects/" + filedir.hexdigest()
+        writefile = open("./.pit/objects/" + filedir.hexdigest()
             + "/" + hashname.hexdigest(),"w")
         with open(sys.argv[2]) as myfile:
             text = myfile.read()
